6b63ad9ea60dfe65769a1ebc709301e3134a8a0b
Aakena <srinivasaaken@gmail.com>
2021-11-03 16:17:32 +0530

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<101;i++)
+            {
+                if(i%3==0 && i % 5 != 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+            }
+        }
+    }
+}
